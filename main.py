b53fde61842b221fa8612fb5d564cbc3f47a8a3f
Felipe Cabrera Ribeiro dos Santos <48259448+felipe-cabrera@users.noreply.github.com>
2020-11-29 18:39:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import adicao
+impot multiplicacao
 
 numero1 = 10
 numero2 = 20
 
-resultado = adicao.adicao(numero1, numero2)
+resultado_adc = adicao.adicao(numero1, numero2)
+resultado_mult = multiplicacao.multiplicacao(numero1, numero2)
 
