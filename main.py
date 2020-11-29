4449e35005c54da528b79e6b5c8fda37ca586d16
Felipe <felipe@bylearn.com.br>
2020-11-29 18:41:40 -0300

    Melhorei o Main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import adicao
-import fatorial
+from adicao import adicao
+from fatorial import fatorial
 
 numero1 = 10
 numero2 = 20
 
-res_adicao = adicao.adicao(numero1, numero2)
+opcao = 1
 
-res_fatorial = fatorail.fatorial(numero1, numero2)
+if opcao == 1:
+    res_adicao = adicao(numero1, numero2)
+elif opcao == 2:
+    res_fatorial = fatorial(numero1, numero2)
